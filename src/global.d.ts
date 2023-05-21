56e0340027ba607cfd88425da393b259c5eb7be7
NicolayV <8309925@gmail.com>
2023-05-21 16:34:04 +0300

    some commit

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,10 +1,10 @@
-declare module "*.scss" {
-  const content: Record<string, string>;
-  export default content;
+declare module '*.scss' {
+  const content: Record<string, string>
+  export default content
 }
 
-declare module "*.svg" {
-  import type { HTMLProps, FC } from "react";
+declare module '*.svg' {
+  import type { HTMLProps, FC } from 'react'
 
-  export const ReactComponent: FC<HTMLProps<SVGElement>>;
+  export const ReactComponent: FC<HTMLProps<SVGElement>>
 }
